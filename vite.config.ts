43e38bfd6cbea0360d2dc0fad45669ab9054a07c
Kawakami Yakumo <kawakami.yakumo@revamp.co.jp>
2025-01-28 09:14:04 +0900

    feat: CNJZ-3346 パスのエイリアス設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: { host: "0.0.0.0", port: 3001 },
+export default defineConfig((mode) => {
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@/": `${__dirname}/src/`,
+      },
+    },
+    envDir: "env",
+    envPrefix: "DP_",
+    server: { host: "0.0.0.0", port: 3001 },
+  };
 });
